54dfbc521d9831bb6daf78400f39eb83a3b290f2
Yaniv Rodenski <roadan@gmail.com>
2021-04-09 11:33:18 +1000

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-while getopts n:s:l:r:k: flag
+while getopts n:s:l:r:k:o:i: flag
 do
     case "${flag}" in
         n) name=${OPTARG};;
         s) storagename=${OPTARG};;    #blacktransformer2
         l) location=${OPTARG};;       #australiaeast
-        r) resourcegroup=${OPTARG};;  #blacktransformer2
+        r) resourcegroup=${OPTARG};;  #blacktransf  ormer2
+        o) badookoutstore=${OPTARG};; 
+        i) incontainer=${OPTARG};; 
     esac
 done
 
@@ -14,56 +16,83 @@ echo "name: $name";
 echo "storagename: $storagename";
 echo "location: $location";
 echo "resourcegroup: $resourcegroup";
-
-# az storage account create \
-#   --name $storagename \
-#   --location australiaeast \
-#   --resource-group bdktransformers \s
-
-#   --sku Standard_LRS
+echo "out container: $badookoutstore";
+echo "in container: $incontainer";
 
 connectionString=$(az storage account show-connection-string --resource-group $resourcegroup --name $storagename --query connectionString --output tsv)
+storageKey=$(az storage account keys list --account-name $storagename --query '[0].value')
 
 az functionapp plan create \
   --resource-group $resourcegroup \
-  --name $name \
+  --name badook-func \
   --location $location \
   --number-of-workers 1 \
-  --sku EP1 \
+  --sku EP3 \
   --is-linux
 
-az functionapp create \
+funcid=$(az functionapp create \
   --name $name \
   --storage-account $storagename \
-  --plan $name \
+  --plan badook-func \
   --resource-group $resourcegroup \
   --os-type Linux \
   --runtime python \
   --runtime-version 3.7 \
   --functions-version 3 \
-  --disable-app-insights \
-  --deployment-container-image-name bdktransformers.azurecr.io/badook/blacktransformer:latest
-
-az functionapp config appsettings set \
-  -n $name \
-  -g $resourcegroup \
-  --settings "AzureWebJobsStorage=$connectionString"
+  --deployment-container-image-name gcr.io/badook-cloud-public/blacktransformer:latest --query id --output tsv 2> /dev/null)
 
 az functionapp config appsettings set \
   -n $name \
   -g $resourcegroup \
   --settings "badookinstore=$connectionString"
 
-# az storage share create --account-name $storagename --account-key $key --name sqlitefiles
+az functionapp config appsettings set \
+  -n $name \
+  -g $resourcegroup \
+  --settings "badookoutstore=$badookoutstore"
 
-# az storage directory create --account-name $storagename --account-key $key --share-name sqlitefiles --name incoming
+az functionapp config appsettings set \
+  -n $name \
+  -g $resourcegroup \
+  --settings "AZURE_BLOB_ACCOUNT_NAME=$storagename"
+  
+az functionapp config appsettings set \
+  -n $name \
+  -g $resourcegroup \
+  --settings "AZURE_BLOB_ACCOUNT_KEY=$storageKey"
 
-# az webapp config storage-account add \
-#   --resource-group $resourcegroup \
-#   --name $name \
-#   --custom-id tranformerId \
-#   --storage-type AzureFiles \
-#   --share-name sqlitefiles \
-#   --account-name $storagename \
-#   --mount-path /bdk \
-#   --access-key XijJFcWz5m3o14HRr/D35zyuJYV6ozxpJZNOcJEq/b7lrDe8AcNfFzCzlM0Cdmhgi6v0ha1uaMHuF6Jioz7Vcw==
+az functionapp config appsettings set \
+  -n $name \
+  -g $resourcegroup \
+  --settings "incontainer=$incontainer"
+  
+az functionapp config appsettings set --name $name \
+  --resource-group $resourcegroup \
+  --settings SCALE_CONTROLLER_LOGGING_ENABLED=AppInsights:Verbose
+
+az functionapp config appsettings set --name $name \
+  --resource-group $resourcegroup \
+  --settings FUNCTIONS_WORKER_PROCESS_COUNT=1
+
+subjectBeginsWith="/blobServices/default/containers/"$incontainer
+storageAccountId=$(az storage account show --name $storagename --resource-group $resourcegroup --query id --output tsv 2> /dev/null)
+
+endpont=${funcid}/functions/new-black-file
+
+sleep 3m
+
+az eventgrid event-subscription create \
+  --name "$name-subscription" \
+  --source-resource-id $storageAccountId \
+  --included-event-types Microsoft.Storage.BlobRenamed Microsoft.Storage.BlobCreated \
+  --endpoint $endpont \
+  --endpoint-type azurefunction \
+  --subject-begins-with $subjectBeginsWith
+
+echo "az eventgrid event-subscription create \
+  --name "$name-subscription" \
+  --source-resource-id $storageAccountId \
+  --included-event-types Microsoft.Storage.BlobRenamed Microsoft.Storage.BlobCreated \
+  --endpoint $endpont \
+  --endpoint-type azurefunction \
+  --subject-begins-with $subjectBeginsWith"
